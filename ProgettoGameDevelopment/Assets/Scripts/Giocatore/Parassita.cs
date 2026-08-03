bbbee5a5b4aed685bf50a6f8b1c8d23bfb479fff
delro-04 <francescodelrosso905@gmail.com>
2026-08-03 19:45:01 +0200

    pawer-up + parassita(non completo)

---
+++ ProgettoGameDevelopment/Assets/Scripts/Giocatore/Parassita.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+
+public class Parassita : MonoBehaviour
+{
+
+    public enum Stato
+    {
+        libero,
+        possessing
+    }
+   public GameObject corpoPosseduto;
+    public Stato StatoAttuale;
+   public bool hasTrojanHorse= false;
+   public bool hasZipBomb =false;
+    private float timerConsumo;
+   public float raggioEsplosione = 5f;
+    public int dannoEsplosione = 50;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {StatoAttuale = Stato.libero;
+       timerConsumo = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if(StatoAttuale == Stato.possessing)
+        {
+            ConsumoPossesso();
+        }
+    }
+public void Possiedi(GameObject corpo)
+    {
+        corpoPosseduto = corpo;
+
+        StatoAttuale = Stato.possessing;
+          timerConsumo=60;
+        
+    }
+
+
+    public void SubisciDanno(int danno)
+    {
+        if(StatoAttuale == Stato.possessing)
+        {
+              timerConsumo = timerConsumo-danno;
+
+            if( timerConsumo<= 0)
+            {
+                
+            }
+        }
+
+    }
+
+
+       private void ConsumoPossesso()
+{
+    timerConsumo += Time.deltaTime;
+
+    if (timerConsumo >= 1f  )
+    {if(hasTrojanHorse){
+        timerConsumo -= 1;
+        
+    }else{
+        timerConsumo -= 2;
+    }
+    }
+}
+
+ 
+ public void EsplosioneZipBomb()
+{
+
+    if(!hasZipBomb){
+        
+        Debug.Log("Zip Bomb non sbloccata");
+        return;
+    }
+    if(StatoAttuale != Stato.possessing){
+    Debug.Log("Zip Bomb non disponibile");
+        return;
+    }
+    Debug.Log("BOOM! Zip Bomb esplosa");
+
+
+    // posizione dell'esplosione
+    Vector3 posizione = corpoPosseduto.transform.position;
+
+
+    // trova tutti i collider nel raggio
+    Collider[] colpiti = Physics.OverlapSphere(
+        posizione,
+        raggioEsplosione
+    );
+
+
+    foreach(Collider c in colpiti)
+    {   
+        if(c.GetComponent<Parassita>() != null)
+    {
+        continue;
+    }
+
+        // bisogna creare una classe padre nemici generica, così che la zip bomb prenda tutti
+        // esempio provvisorio con guard:
+        /*guard umano = c.GetComponent<guard>();
+
+       if(umano != null)
+        {
+            umano.HitPoints -= dannoEsplosione;
+
+            Debug.Log(
+                "Danno Zip Bomb a " + umano.name
+            );
+        }*/
+    }
+
+
+    // distrugge il corpo sacrificato
+    Destroy(corpoPosseduto);
+
+
+    corpoPosseduto = null;
+
+    StatoAttuale = Stato.libero;
+
+
+    
+}
+public void UnlockTrojanHorse()
+{
+    hasTrojanHorse = true;
+    Debug.Log("Trojan Horse sbloccato!");
+}
+public void UnlockZipBomb()
+{
+    hasZipBomb = true;
+    Debug.Log("Zip Bomb sbloccato!");
+}
+private void OnDrawGizmosSelected()
+{
+    Gizmos.DrawWireSphere(
+        transform.position,
+        raggioEsplosione
+    );
+}
+}
+
---
+++ ProgettoGameDevelopment/Assets/Scripts/pawer-up/TrojanHorsePickup.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TrojanHorsePickup : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Parassita parassita = other.GetComponent<Parassita>();
+
+        if (parassita != null)
+        {
+            parassita.UnlockTrojanHorse();
+
+            Destroy(gameObject);
+        }
+    }
+}
