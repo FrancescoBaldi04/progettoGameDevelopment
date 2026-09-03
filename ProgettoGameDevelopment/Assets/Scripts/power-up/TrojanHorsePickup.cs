651dfe6560b5260a61a947d874cba6e1b0324eff
FrancescoBaldi04 <frabaldi2004@gmail.com>
2026-09-03 18:17:26 +0200

    Menu dei comandi completo

--- ProgettoGameDevelopment/Assets/Scripts/power-up/TrojanHorsePickup.cs
+++ ProgettoGameDevelopment/Assets/Scripts/power-up/TrojanHorsePickup.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class TrojanHorsePickup : MonoBehaviour
 {
-    [SerializeField] private string trojanHorseUIdescription;
     private PickupMessage pickupMessage;
 
     private void Start()
