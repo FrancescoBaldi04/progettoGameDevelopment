829a26cbd6ac0df76ec8ed4da71a358d290ae4d4
delro-04 <francescodelrosso905@gmail.com>
2026-09-06 16:30:52 +0200

    scritta blocco

--- ProgettoGameDevelopment/Assets/Scripts/Giocatore/blocco.cs
+++ ProgettoGameDevelopment/Assets/Scripts/Giocatore/blocco.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class blocco : MonoBehaviour
 {
     [SerializeField] private Collider2D Blocco;
+    [SerializeField] private GameObject BlockMessage;
+
+    private Coroutine messageCoroutine;
 
     private void Start()
     {
-        // Il blocco è attivo se NON abbiamo il Trojan Horse
-       Blocco.enabled = true;
+        BlockMessage.SetActive(false);
+        AggiornaBlocco();
     }
 
     private void Update()
@@ -20,9 +24,41 @@ public class blocco : MonoBehaviour
         if (GameManager.gameManager == null)
             return;
 
-        // SENZA Trojan Horse = blocco attivo
-        // CON Trojan Horse = blocco disattivato
-        Blocco.enabled = !GameManager.gameManager.hasTrojanHorse;
+        if (GameManager.gameManager.hasTrojanHorse)
+        {
+            Blocco.enabled = false;
+        }
+        else
+        {
+            Blocco.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (GameManager.gameManager == null)
+            return;
+
+        if (GameManager.gameManager.hasTrojanHorse)
+            return;
+
+        if (other.GetComponent<Parassita>() != null)
+        {
+            if (messageCoroutine != null)
+                StopCoroutine(messageCoroutine);
+
+            messageCoroutine = StartCoroutine(ShowMessage());
+        }
+    }
+
+    private IEnumerator ShowMessage()
+    {
+        BlockMessage.SetActive(true);
+
+        yield return new WaitForSeconds(3f);
+
+        BlockMessage.SetActive(false);
+
+        messageCoroutine = null;
     }
 }
-
